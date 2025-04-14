eaaa527280a9f8642a227365e1ea847602958f09
mfrancis <margarida.matiass@gmail.com>
2025-04-14 19:29:21 +0100

    ex02 done

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,14 +6,26 @@
 /*   By: mfrancis <mfrancis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 16:28:47 by mi-matias         #+#    #+#             */
-/*   Updated: 2025/04/13 22:29:51 by mfrancis         ###   ########.fr       */
+/*   Updated: 2025/04/14 19:28:48 by mfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./zombie.hpp"
+#include <iostream>
+#include <string.h>
 
 int main()
 {
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+    
+    std::cout << "The memory address of the string variable: " << &str <<std::endl;
+    std::cout << "The memory address held by stringPTR: " << stringPTR <<std::endl;
+    std::cout << "The memory address held by stringREF: " <<  &stringREF <<std::endl;
 
+    std::cout << "The value of the string variable: " << str << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR <<std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF <<std::endl;
+        
     return (0);
 }
